38514dc1eb09e3526499f277e55c19ad087eb97a
LabFox <consockbind@gmail.com>
2024-09-29 11:57:24 +0200

    Trying to fix direct pip install

--- setup.py
+++ setup.py
@@ -6,56 +6,6 @@ import subprocess
 import platform
 import os
 
-
-class BuildGoModule(install):
-    def run(self):
-        # Ensure the Go module is built before the Python package
-        self.build_go_module()
-        super().run()
-
-    def build_go_module(self):
-        # Define the Go build command, something like
-        # GOOS=darwin GOARCH=amd64 go build -buildmode=c-shared -o ./whatsmeow/whatsmeow-darwin-amd64.dylib main.go
-        # Detect the current OS and architecture
-        current_os = platform.system().lower()
-        current_arch = platform.machine().lower()
-
-        # Map the architecture to Go's naming convention
-        arch_map = {
-            "x86_64": "amd64",
-            "arm64": "arm64",
-            "aarch64": "arm64",
-        }
-        go_arch = arch_map.get(current_arch, current_arch)
-
-        # Set the environment variables for Go build
-        env = os.environ.copy()
-        env["GOOS"] = current_os
-        env["GOARCH"] = go_arch
-
-        go_build_cmd = [
-            "go",
-            "build",
-            "-buildmode=c-shared",
-            "-o",
-            f"whatsmeow/whatsmeow-{current_os}-{go_arch}.dylib",
-            "main.go",
-        ]
-        print(
-            f"building Go module with command: {' '.join(go_build_cmd)} in directory {os.getcwd()}/whatsfly/dependencies"
-        )
-        # Run the Go build command
-        try:
-            status_code = subprocess.check_call(go_build_cmd, cwd="whatsfly/dependencies")
-        except:
-            print("Wheel build failed, will retry on runtime")
-            return
-
-        print(f"Go build command exited with status code: {status_code}")
-        if status_code != 0:
-            print("Wheel build failed, will retry on runtime")
-
-
 setup(
     name="whatsfly",
     version="0.1.3",
@@ -68,9 +18,6 @@ setup(
     packages=find_packages(),
     install_reqs = ["types-PyYAML", "setuptools", "requests", "qrcode"],
     include_package_data=True,
-    cmdclass={
-        "install": BuildGoModule,
-    },
     classifiers=[
         "Intended Audience :: Developers",
         "Natural Language :: English",
